7a42630a7a5aaa8162eecc9d98d6567ab780f285
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2020-08-25 08:46:12 -0400

    Create skeleton of TransparentList class

---
+++ transparent_list.py
@@ -0,0 +1,26 @@
+from list_node import ListNode
+
+class TransparentList:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def get(self, idx):
+        pass
+
+    def insert(self, val, idx = 0):
+        pass
+
+    def set(self, val, idx):
+        pass
+
+    def remove(self, idx):
+        pass
+
+    def getNode(self, idx):
+        pass
