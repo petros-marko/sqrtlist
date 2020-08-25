e9ef1ccf7029a83008c17410636822d629d2817a
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2020-08-25 08:53:29 -0400

    Create skeleton of SqrtList class

---
+++ sqrtlist.py
@@ -0,0 +1,37 @@
+from list_node import ListNode
+from transparent_list import TransparentList
+from math import sqrt, floor
+
+class SqrtList:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def buildMeta(self):
+        pass
+
+    def getNode(self, idx):
+        pass
+
+    def advance(self, removalIdx):
+        pass
+
+    def retreat(self, insertionIdx):
+        pass
+
+    def get(self, idx):
+        pass
+
+    def set(self, val, idx):
+        pass
+
+    def insert(self, val, idx = 0):
+        pass
+
+    def remove(self, idx):
+        pass
