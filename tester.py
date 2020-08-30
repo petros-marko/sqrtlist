2f273e4f7515d1cc6d287fed50c361e10b66a94d
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2020-08-30 08:59:02 -0400

    Comment code

--- tester.py
+++ tester.py
@@ -1,17 +1,25 @@
 from sqrtlist import SqrtList
 from random import randint
 
+#simple utility for testing the correctness of list operations on sqrtlist
+
 tester = []
 testee = SqrtList()
 
+#perform 1e5 operations on the python list and the sqrtlist and check for any mismatch on their effect
+#there should be none
+#each time an operation is decided randomly with equal probability
+#if the operation chosen is not possible on the current list, it is just skipped
 for i in range(100000):
     cmd = randint(0, 3)
+    #insert operation
     if cmd == 0:
         wh = randint(0, len(tester))
         tester.insert(wh, i)
         testee.insert(i, wh)
         if str(tester) != str(testee).split(' || ')[0]:
             print('insertion error')
+    #remove operation
     elif cmd == 1:
         if len(tester) < 1:
             continue
@@ -20,12 +28,14 @@ for i in range(100000):
         testee.remove(wh)
         if str(tester) != str(testee).split(' || ')[0]:
             print('deletion error')
+    #get operation
     elif cmd == 2:
         if len(tester) < 1:
             continue
         wh = randint(0, len(tester) - 1)
         if tester[wh] != testee.get(wh):
             print('access error')
+    #set operation
     else:
         if len(tester) < 1:
             continue
